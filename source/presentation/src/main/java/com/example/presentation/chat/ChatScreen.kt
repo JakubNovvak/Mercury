cb24f43ddaffa1a21184c5cfb449c2ee68f348bd
JakubNovvak <jakubszymonnowak@gmail.com>
2025-09-23 22:49:54 +0200

    feat: add empty Composables for testing purposes
    Add three composables, related to login, list of chars and specific chat screens. Fill them with some placeholder text.

---
+++ source/presentation/src/main/java/com/example/presentation/chat/ChatScreen.kt
@@ -0,0 +1,9 @@
+package com.example.presentation.chat
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ChatScreen(): Unit {
+    Text("Welcome to Chat Screen!")
+}
