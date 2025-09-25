197a518ec6e0606fd889214d2ba680837cad393c
JakubNovvak <jakubszymonnowak@gmail.com>
2025-09-25 13:25:14 +0200

    feat: add getMessagesByChatId to ChatRepository and move mocked data to standalone file
    Expand ChatRepository by additional function, to give ability of acquiring messages of the chosen chat. Move mocked chats and messages to the separate files and access them from th ChatRepository, for better readability.

--- source/data/src/main/java/com/example/data/mock/MockChatRepository.kt
+++ source/data/src/main/java/com/example/data/mock/MockChatRepository.kt
@@ -1,16 +1,13 @@
 package com.example.data.mock
 
 import com.example.domain.entities.Chat
+import com.example.domain.entities.Message
 import com.example.domain.interfaces.ChatRepository
+import com.example.domain.mock.mockChats
 
 //The data is mocked for only one User, with id 1
 class MockChatRepository: ChatRepository {
-    private val availableChats = listOf<Chat>(
-        Chat(id = "c1", name = "Test Testowy", participantsIds = listOf("1", "2")),
-        Chat(id = "c2", name = "Jan Kowalski", participantsIds = listOf("1", "3")),
-        Chat(id = "c3", name = "Adam Gessler", participantsIds = listOf("1", "4")),
-        Chat(id = "c4", name = "John Smith", participantsIds = listOf("1", "5"))
-    )
+    private var availableChats = mockChats
 
     override fun getChats(): List<Chat> {
         return availableChats
@@ -18,5 +15,9 @@ class MockChatRepository: ChatRepository {
 
     override fun getChatById(id: String): Chat? {
         return availableChats.firstOrNull { c -> c.id == id }
+    }
+
+    override fun getMessagesByChatId(chatId: String): List<Message> {
+        return availableChats.firstOrNull { it.id == chatId }!!.chatMessages
     }
 }
--- source/domain/src/main/java/com/example/domain/interfaces/ChatRepository.kt
+++ source/domain/src/main/java/com/example/domain/interfaces/ChatRepository.kt
@@ -1,8 +1,10 @@
 package com.example.domain.interfaces
 
 import com.example.domain.entities.Chat
+import com.example.domain.entities.Message
 
 interface ChatRepository {
     fun getChats(): List<Chat>
     fun getChatById(id: String): Chat?
+    fun getMessagesByChatId(chatId: String): List<Message>
 }
