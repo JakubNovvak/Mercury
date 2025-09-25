c032c8795d9da1dd4af49602831aac7a917817d8
JakubNovvak <jakubszymonnowak@gmail.com>
2025-09-25 13:27:28 +0200

    feat: implement composable representing chat in list and related to it ui model

---
+++ source/presentation/src/main/java/com/example/presentation/chatlist/components/ChatListElement.kt
@@ -0,0 +1,67 @@
+package com.example.presentation.chatlist.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ChatListElement(
+    title: String,
+    lastMessageTime: String,
+    lastMessagePreview: String,
+): Unit {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Start,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Image(
+            imageVector = Icons.Filled.AccountCircle,
+            contentDescription = "Account icon",
+            modifier = Modifier
+                .padding(end = 10.dp)
+                .size(60.dp)
+        )
+
+        Column {
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = title,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.W600,
+                    modifier = Modifier.padding(bottom = 5.dp)
+                )
+
+                Text(
+                    text = lastMessageTime,
+                    color = Color.Gray
+                )
+            }
+
+            Text(
+                text = lastMessagePreview,
+                color = Color.Gray
+            )
+        }
+    }
+}
